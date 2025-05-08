0d8f3fbcb5ca97723013060b81f66ae9a3f6caa3
Roman <Sapunovrm@bk.ru>
2025-05-08 21:40:10 +0500

    Django_2.1_migrations_решено

--- school/admin.py
+++ school/admin.py
@@ -1,13 +1,16 @@
 from django.contrib import admin
-
 from .models import Student, Teacher
 
 
 @admin.register(Student)
 class StudentAdmin(admin.ModelAdmin):
-    pass
+    list_display = ['id', 'name', 'group', 'display_teachers']
+
+    def display_teachers(self, obj):
+        return ", ".join([teacher.name for teacher in obj.teachers.all()])
+    display_teachers.short_description = 'Учителя'
 
 
 @admin.register(Teacher)
 class TeacherAdmin(admin.ModelAdmin):
-    pass
+    list_display = ['id', 'name', 'subject']
